02dc3ace7c9cce662c3b046c675a35cc6c03ccd1
cmeynieux <meynieux.cecile@gmail.com>
2022-05-07 03:09:06 +0200

    init project spring and angular

---
+++ backend/src/main/java/com/isep/pamagace/PamagaceApplication.java
@@ -0,0 +1,13 @@
+package com.isep.pamagace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamagaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamagaceApplication.class, args);
+	}
+
+}
