417bb4798ed2d718bc4f31d761f6a6a5c108380d
cmeynieux <meynieux.cecile@gmail.com>
2022-05-07 15:16:27 +0200

    create DOA interface for each entity

---
+++ backend/src/main/java/com/isep/pamagace/user/IUserDao.java
@@ -0,0 +1,9 @@
+package com.isep.pamagace.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserDao extends JpaRepository<User, Integer> {
+
+}
