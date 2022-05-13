908398190010535d6890068032e795c1c60cacb8
cmeynieux <meynieux.cecile@gmail.com>
2022-05-13 22:30:13 +0200

    Authentication in progress

--- backend/src/main/java/com/isep/pamagace/user/User.java
+++ backend/src/main/java/com/isep/pamagace/user/User.java
@@ -1,17 +1,27 @@
 package com.isep.pamagace.user;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "user")
 @Data
-public class User {
+public class User implements Serializable, UserDetails {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_user;
+
+    @Column(name = "username", nullable = false)
+    private String username;
 
     @Column(name = "firstname", nullable = false)
     private String firstname;
@@ -28,4 +38,49 @@ public class User {
     @Column(name = "birthday", nullable = false)
     private Date birthday;
 
+    @Column(name="authorization", nullable = false)
+    private String authorization;
+
+    public User(String username, String firstname, String lastname, String emailadress, String password, Date birthday, String authorization) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.emailadress = emailadress;
+        this.password = password;
+        this.birthday = birthday;
+        this.authorization = authorization;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        GrantedAuthority authority = new SimpleGrantedAuthority(authorization);
+        grantList.add(authority);
+        return grantList;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
 }
--- backend/src/main/java/com/isep/pamagace/user/UserRestController.java
+++ backend/src/main/java/com/isep/pamagace/user/UserRestController.java
@@ -1,7 +1,30 @@
 package com.isep.pamagace.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserRestController {
+    @Autowired
+    private IUserDao userDao;
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping(value="/user", method = RequestMethod.GET)
+    public String hello(@Param("name") String name){
+        return "Hello" + name;
+    }
+
+    @RequestMapping(value="/users", method = RequestMethod.GET)
+    public List<User> getUsers(){
+        return userDao.findAll();
+    }
+
+    @PostMapping(value="/addUser")
+    public User addUser(@RequestBody User user){
+        return userService.saveOrUpdateUser(user);
+    }
 }
