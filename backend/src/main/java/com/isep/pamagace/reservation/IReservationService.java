dfb7a1835e13ba645bd194695ddcb385aa959ddd
GaranceK <garancekrettly@gmail.com>
2022-05-23 14:47:52 +0200

    reservation

--- backend/src/main/java/com/isep/pamagace/reservation/IReservationService.java
+++ backend/src/main/java/com/isep/pamagace/reservation/IReservationService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -17,7 +18,7 @@ public interface IReservationService extends JpaRepository<Reservation, Integer>
     @Modifying
     @Query(value="INSERT INTO Reservation (id_user, id_house, date_start, date_end, grade_house, grade_host, grade_visitor, status) VALUES (?1, ?2, ?3, ?4, ?5,?6,?7,?8);",
             nativeQuery = true)
-    List<Reservation> insertReservation(int id_user, int id_house, Date date_start, Date date_end, int grade_house, int grade_host, int grade_visitor, int status);
+    Void insertReservation(int id_user, int id_house, LocalDate date_start, LocalDate date_end, int grade_house, int grade_host, int grade_visitor, int status);
 
 
 }
--- backend/src/main/java/com/isep/pamagace/reservation/ReservationRestController.java
+++ backend/src/main/java/com/isep/pamagace/reservation/ReservationRestController.java
@@ -3,32 +3,38 @@ package com.isep.pamagace.reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
 @RestController
 public class ReservationRestController {
+
     @Autowired
     private IReservationDao reservationDao;
     @Autowired
     private IReservationService reservationService;
 
-    @RequestMapping(value = "/reservation/get/all", method = RequestMethod.GET)
+    @RequestMapping(value = "api/public/reservation/get/all", method = RequestMethod.GET)
     public List<Reservation> getReservations() {
         return reservationDao.findAllReservations();
     }
 
-    @RequestMapping(value = "/reservation/get/by/id/{reservation_id}", method = RequestMethod.GET)
+    @RequestMapping(value = "api/public/reservation/get/by/id/{reservation_id}", method = RequestMethod.GET)
     public Reservation getReservationByID(@PathVariable("reservation_id") int reservation_id) {
         return reservationDao.findReservationById(reservation_id);
     }
 
-    @RequestMapping(value = "/reservation/post", method = RequestMethod.POST)
-    public List<Reservation> postReservation(@RequestParam("id_user") int id_user, @RequestParam("id_house") int id_house,
-                                             @RequestParam("date_start") Date date_start, @RequestParam("date_end") Date date_end,
+    @RequestMapping(value = "api/public/reservation/post", method = RequestMethod.POST)
+    public void postReservation(@RequestParam("id_user") int id_user, @RequestParam("id_house") int id_house,
+                                             @RequestParam("date_start") String date_start, @RequestParam("date_end") String date_end,
                                              @RequestParam("grade_house") int grade_house, @RequestParam("grade_host") int grade_host,
                                              @RequestParam("grade_visitor") int grade_visitor, @RequestParam("status") int status) {
-        return reservationService.insertReservation(id_user, id_house, date_start, date_end, grade_house, grade_host, grade_visitor, status);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date_s = LocalDate.parse(date_start,formatter);
+        LocalDate date_e = LocalDate.parse(date_end,formatter);
+        reservationService.insertReservation(id_user, id_house, date_s, date_e, grade_house, grade_host, grade_visitor, status);
     }
 
 
