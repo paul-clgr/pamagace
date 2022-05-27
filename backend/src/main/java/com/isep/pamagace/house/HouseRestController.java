2ddf333091b7c3d316e50c1dce1f297df76551a3
cmeynieux <57501739+Cecile13122@users.noreply.github.com>
2022-05-27 15:55:14 +0200

    add crossOrigin

--- backend/src/main/java/com/isep/pamagace/house/HouseRestController.java
+++ backend/src/main/java/com/isep/pamagace/house/HouseRestController.java
@@ -5,13 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+
 @RequestMapping("/api/public/house")
 public class HouseRestController {
 	@Autowired
