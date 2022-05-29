cecaa4c30d9f4908ae41c10ae63ee95bc7ca4ece
cmeynieux <57501739+Cecile13122@users.noreply.github.com>
2022-05-30 00:59:44 +0200

    add backoffice criterias

--- backend/src/main/java/com/isep/pamagace/criteria/CriteriaRestController.java
+++ backend/src/main/java/com/isep/pamagace/criteria/CriteriaRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api/public/criteria")
+//@RequestMapping("/api/public/criteria")
 public class CriteriaRestController {
 	@Autowired
 	private ICriteriaDao criteriaDao;
@@ -23,16 +23,21 @@ public class CriteriaRestController {
 	public CriteriaRestController() {
 	}
 
-	@GetMapping("/all")
+	@GetMapping("/api/public/criteria/all")
 	List<Criteria> all() {
 		return criteriaDao.findAll();
 	}
 	
-	@GetMapping("/id")
+	@GetMapping("/api/public/criteria/id")
 	Optional<Criteria> byID(Integer id) {
 		return criteriaDao.findById(id);
 	}
 
-	@PostMapping("/addCriteria")
+	@PostMapping("/api/admin/criteria/addCriteria")
 	Criteria addCriteria(@RequestBody Criteria newCriteria){ return criteriaService.saveOrUpdateCritera(newCriteria);}
+
+	@GetMapping("/api/admin/criteria/deleteCriteria/{id}")
+	public void deleteCriteriaById(@PathVariable(value = "id") int id) {
+		criteriaDao.deleteById(id);
+	}
 }
